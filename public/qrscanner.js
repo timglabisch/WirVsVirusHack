4734dc21a389c99970316bef01486ae72ad94f51
timglabisch <glabisch@easybill.de>
2020-03-21 14:32:01 +0100

    tidy up a bit

--- public/qrscanner.js
+++ public/qrscanner.js
@@ -6,13 +6,64 @@ var outputContainer = document.getElementById("output");
 var outputMessage = document.getElementById("outputMessage");
 var outputData = document.getElementById("outputData");
 
-function drawLine(begin, end, color) {
-    canvas.beginPath();
-    canvas.moveTo(begin.x, begin.y);
-    canvas.lineTo(end.x, end.y);
-    canvas.lineWidth = 4;
-    canvas.strokeStyle = color;
-    canvas.stroke();
+class QR {
+    constructor(video, canvas) {
+        this.onNewData = null;
+        this.video = video;
+        requestAnimationFrame(this.tick.bind(this));
+    }
+
+    onNewData(cb) {
+        this.onNewData = cb;
+    }
+
+    drawLine(begin, end, color) {
+        canvas.beginPath();
+        canvas.moveTo(begin.x, begin.y);
+        canvas.lineTo(end.x, end.y);
+        canvas.lineWidth = 4;
+        canvas.strokeStyle = color;
+        canvas.stroke();
+    }
+
+    tick() {
+        loadingMessage.innerText = "⌛ Loading video...";
+        if (this.video.readyState === video.HAVE_ENOUGH_DATA) {
+            loadingMessage.hidden = true;
+            canvasElement.hidden = false;
+            outputContainer.hidden = false;
+
+            canvasElement.height = this.video.videoHeight;
+            canvasElement.width = this.video.videoWidth;
+            canvas.drawImage(this.video, 0, 0, canvasElement.width, canvasElement.height);
+            var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
+            var code = jsQR(imageData.data, imageData.width, imageData.height, {
+                inversionAttempts: "dontInvert",
+            });
+            if (code) {
+                console.log('is code...');
+                drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
+                drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
+                drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
+                drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
+                outputMessage.hidden = true;
+                outputData.parentElement.hidden = false;
+                outputData.innerText = code.data;
+                if(!outputContainer.classList.contains("redbg")) {
+                    outputContainer.classList.add("redbg")
+                }
+            } else {
+                console.log('no code...');
+                outputMessage.hidden = true;
+                outputData.parentElement.hidden = false;
+                if(outputContainer.classList.contains("redbg")) {
+                    outputContainer.classList.remove("redbg")
+                }
+            }
+        }
+        requestAnimationFrame(this.tick.bind(this));
+    }
+
 }
 
 // Use facingMode: environment to attemt to get the front camera on phones
@@ -20,41 +71,6 @@ navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).th
     video.srcObject = stream;
     video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
     video.play();
-    requestAnimationFrame(tick);
+    (new QR(video))
 });
 
-function tick() {
-    loadingMessage.innerText = "⌛ Loading video..."
-    if (video.readyState === video.HAVE_ENOUGH_DATA) {
-        loadingMessage.hidden = true;
-        canvasElement.hidden = false;
-        outputContainer.hidden = false;
-
-        canvasElement.height = video.videoHeight;
-        canvasElement.width = video.videoWidth;
-        canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
-        var imageData = canvas.getImageData(0, 0, canvasElement.width, canvasElement.height);
-        var code = jsQR(imageData.data, imageData.width, imageData.height, {
-            inversionAttempts: "dontInvert",
-        });
-        if (code) {
-            drawLine(code.location.topLeftCorner, code.location.topRightCorner, "#FF3B58");
-            drawLine(code.location.topRightCorner, code.location.bottomRightCorner, "#FF3B58");
-            drawLine(code.location.bottomRightCorner, code.location.bottomLeftCorner, "#FF3B58");
-            drawLine(code.location.bottomLeftCorner, code.location.topLeftCorner, "#FF3B58");
-            outputMessage.hidden = true;
-            outputData.parentElement.hidden = false;
-            outputData.innerText = code.data;
-            if(!outputContainer.classList.contains("redbg")) {
-                outputContainer.classList.add("redbg")
-            }
-        } else {
-            outputMessage.hidden = true;
-            outputData.parentElement.hidden = false;
-            if(outputContainer.classList.contains("redbg")) {
-                outputContainer.classList.remove("redbg")
-            }
-        }
-    }
-    requestAnimationFrame(tick);
-}
